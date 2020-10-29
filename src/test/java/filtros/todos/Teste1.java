a34a969d7a26a16d124ea95497baec8ea1d61dd9
Diego <diego.araujo@nava.com.br>
2020-10-29 20:44:54 -0300

    Teste Automação Visões

--- src/test/java/filtros/todos/Teste1.java
+++ src/test/java/filtros/todos/Teste1.java
@@ -17,48 +17,64 @@ public class Teste1 extends Filtros {
 		public void Test() {
 			WebDriver driver = new ChromeDriver();
 			List<ViewBTDTO> test = new ArrayList<ViewBTDTO>(); 
-			ViewBTDTO BT = new ViewBTDTO();
-			BT.setUrl("https://192.168.80.18:8081/mir.console/pages/view/viewbtbrand.jsf");
-			BT.setTable("treeViewBTBrandDTO_data");
-			test.add(BT);
-			driver.get(test.get(0).getUrl());
-			// driver.get("https://10.59.184.60/mir.console/pages/view/viewbtbrand.jsf");
-			// Erro de Privacidade
-			click(driver, "details-button");
-			click(driver, "proceed-link");
-			waitingtoclick(driver, "formLogin:txtUsuario");
-			// Login
-			click(driver, "formLogin:txtUsuario");
-			escreverlogin(driver, "formLogin:txtUsuario");
-			escreversenhaenter(driver, "formLogin:txtPassword");
-			// Filtro
-			waitingtoclick(driver, "bntSearch");
-			click(driver, "bntSearch");
-			// Preencher Periodo
-			waitingtoclickx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[1]/input");
-			writeperiod1x(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[1]/input");
-			clickx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[2]/input");
-			writeperiod2x(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[2]/input");
-			// Preencher novos campos
-			writepanx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[1]/div/input");
-			writecvvx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[2]/div/input");
-			writedescx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[3]/div/input");
-			writeportx(driver,
-					"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[4]/div/input");
-			// Consultar
-			click(driver, "btnSearch1");
-			waiting(driver, "dialogLoadbar");
-			// Verificar se possui dados
-			notequalsid(driver, test.get(0).getTable());
-			// Verificar se ocorre erro
-			erroDesc(driver, "/html/body/div[8]/div/div/div[2]/span");
-			driver.quit();
+			ViewBTDTO bt = new ViewBTDTO();
+			ViewBTDTO bt2 = new ViewBTDTO();
+			ViewBTDTO bt3 = new ViewBTDTO();
+			bt.setUrl("https://192.168.80.18:8081/mir.console/pages/view/viewbtbrand.jsf");
+			bt.setTable("treeViewBTBrandDTO_data");
+			bt.setUrl("https://192.168.80.18:8081/mir.console/pages/view/viewissuer.jsf");
+			bt.setTable("listViewBTIssuerDTO_data");
+			bt.setUrl("https://192.168.80.18:8081/mir.console/pages/view/viewproduct.jsf");
+			bt.setTable("treeViewBTSubProductDTO_data");
+			test.add(bt);
+			test.add(bt2);
+			test.add(bt3);
+
+			for ( ViewBTDTO obj : test){
+				driver.get(test.get(0).getUrl());
+				// driver.get("https://10.59.184.60/mir.console/pages/view/viewbtbrand.jsf");
+				// Erro de Privacidade
+				click(driver, "details-button");
+				click(driver, "proceed-link");
+				waitingtoclick(driver, "formLogin:txtUsuario");
+				// Login
+				click(driver, "formLogin:txtUsuario");
+				escreverlogin(driver, "formLogin:txtUsuario");
+				escreversenhaenter(driver, "formLogin:txtPassword");
+				// Filtro
+				waitingtoclick(driver, "bntSearch");
+				click(driver, "bntSearch");
+				// Preencher Periodo
+				waitingtoclickx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[1]/input");
+				//Desflaga o Agrupamento por bandeiras quando é tela de visão emissor
+				if(test.get(0).getUrl()=="https://192.168.80.18:8081/mir.console/pages/view/viewissuer.jsf"){
+					clickx(driver, "/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[2]/div");
+				}
+				writeperiod1x(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[1]/input");
+				clickx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[2]/input");
+				writeperiod2x(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[1]/div[1]/div/span[2]/input");
+				// Preencher novos campos
+				writepanx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[1]/div/input");
+				writecvvx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[2]/div/input");
+				writedescx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[3]/div/input");
+				writeportx(driver,
+						"/html/body/center/div[3]/form[1]/div[3]/div[2]/div[1]/div/div[2]/div/div[5]/div[4]/div/input");
+				// Consultar
+				click(driver, "btnSearch1");
+				waiting(driver, "dialogLoadbar");
+				// Verificar se possui dados
+				notequalsid(driver, test.get(0).getTable());
+				// Verificar se ocorre erro
+				erroDesc(driver, "/html/body/div[8]/div/div/div[2]/span");
+				driver.quit();
+			}
+
 		}
 }
