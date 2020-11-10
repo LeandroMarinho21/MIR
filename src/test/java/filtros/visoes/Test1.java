13e04d34717db0d4ac48b62878d87a27536f6785
LeandroMarinho21 <leandro.tmarinho21@gmail.com>
2020-11-10 14:50:46 -0300

    Final

--- src/test/java/filtros/visoes/Test1.java
+++ src/test/java/filtros/visoes/Test1.java
@@ -40,7 +40,8 @@ public class Test1 extends Filtros {
 		// Verificar se possui dados
 		wait3(driver, "");
 		System.out.println("Entrando");
-		notequals(driver, "//*[contains(@class, 'ui-widget-content ui-treetable-empty-message')]");
+		notequals(driver, "//*[contains(@class, 'ui-treetable-data ui-widget-content')]");
+		notequals2(driver, "//*[contains(@class, 'ui-treetable-data ui-widget-content')]");
 		System.out.println("Sai");
 		// Verificar se ocorre erro
 		erroDesc(driver, "/html/body/div[8]/div/div/div[2]/span");
