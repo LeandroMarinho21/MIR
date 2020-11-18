a160f0704bcc1f0e1e519bdb326a7ab51891f567
LeandroMarinho21 <leandro.tmarinho21@gmail.com>
2020-11-18 13:37:08 -0300

    Login

--- src/test/java/visao/redes/FiltrosRedes.java
+++ src/test/java/visao/redes/FiltrosRedes.java
@@ -20,7 +20,7 @@ public class FiltrosRedes extends Filtros {
 		driver = new ChromeDriver();
 		List<ViewBTDTO> links = getViews();
 		/// Login
-		login(driver, links.get(4).getUrl());
+		login2(driver, links.get(4).getUrl());
 		// Filtro
 		waitingtoclick(driver, "btnFilter");
 		click(driver, "btnFilter");
