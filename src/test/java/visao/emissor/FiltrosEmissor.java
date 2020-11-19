edb20361e4f033d11ca33a33e1de3dbab3bee016
LeandroMarinho21 <leandro.tmarinho21@gmail.com>
2020-11-18 21:24:20 -0300

    Reestruturação Emissor

--- src/test/java/visao/emissor/FiltrosEmissor.java
+++ src/test/java/visao/emissor/FiltrosEmissor.java
@@ -38,393 +38,145 @@ public class FiltrosEmissor extends Filtros {
 		driver.quit();
 	}
 
-	/// Objetivo: Preencher o combo Emissores
+	/// Objetivo: Preencher os Combos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorEmissores() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_Issuer')]");
+	public void TestCombos1() {
+		// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_Issuer')]");
-		clickx(driver, "//div[32]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Subproduto
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorSubproduto() {
-		// Preencher Combobox
+		clickx(driver, "//div[30]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_SubProduct')]");
 		clickx(driver, "//div[contains(@class,'entity_SubProduct')]");
-		clickx(driver, "//div[33]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Quantidade de Parcelas
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+		clickx(driver, "//div[31]/div[1]/div[1]");
+		}
+		
+		/// Objetivo: Preencher os Combos
+		/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
+		
 	@Test
-	public void TestEmissorQtdParcelas() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_Plot')]");
+	public void TestCombos2() {
+			// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_Plot')]");
-		clickx(driver, "//div[34]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Operadoras
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorOperadoras() {
-		// Preencher Combobox
+		clickx(driver, "//div[32]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_CellphoneOperator')]");
 		clickx(driver, "//div[contains(@class,'entity_CellphoneOperator')]");
-		clickx(driver, "//div[35]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Bandeira
+		clickx(driver, "//div[33]/div[1]/div[1]");
+		}
+	
+	/// Objetivo: Preencher o Campos dos 11 campos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorBandeira() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_Brand')]");
-		clickx(driver, "//div[contains(@class,'entity_Brand')]");
-		clickx(driver, "//div[36]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o campo Imei com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorImei() {
-		// Preencher Combobox
+	public void TestNovosCampos1() {
+		// Preencher Campos
 		writeimei(driver, "//input[contains(@class,'entity_imei')]");
-	}
-
-	/// Objetivo: Preencher o campo SimCard com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorSimcard() {
-		// Preencher Combobox
 		writesimcard(driver, "//input[contains(@class,'entity_simCard')]");
-	}
-
-	/// Objetivo: Preencher o campo Nome Operadora com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorNomeOperadora() {
-		// Preencher Combobox
 		writeoperadora(driver, "//input[contains(@class,'entity_nmOpe')]");
-	}
-
-	/// Objetivo: Preencher o campo Mac Address com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorMacAddress() {
-		// Preencher Combobox
 		writemacaddress(driver, "//input[contains(@class,'entity_macAdr')]");
-	}
-
-	/// Objetivo: Preencher o campo Ip Terminal com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorIpTerminal() {
-		// Preencher Combobox
 		writeipterminal(driver, "//input[contains(@class,'entity_ipTerm')]");
-	}
-
-	/// Objetivo: Preencher o campo Codigo do pais para celular com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodPaisParaCel() {
-		// Preencher Combobox
 		writecodpais(driver, "//input[contains(@class,'entity_cgiMcc')]");
-	}
-
-	/// Objetivo: Preencher o campo Codigo de rede movel com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodRede() {
-		// Preencher Combobox
 		writecodrede(driver, "//input[contains(@class,'entity_cgiMnc')]");
-	}
-
-	/// Objetivo: Preencher o campo Codigo de area de localizacao com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodArea() {
-		// Preencher Combobox
 		writecodarea(driver, "//input[contains(@class,'entity_cgiLac')]");
-	}
-
-	/// Objetivo: Preencher o campo Identidade do celular com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorIdtDoCel() {
-		// Preencher Combobox
 		writeidtcel(driver, "//input[contains(@class,'entity_cgiCI')]");
-	}
-
-	/// Objetivo: Preencher o campo Versao da plataforma com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorVersaoPlataforma() {
-		// Preencher Combobox
 		writeversionplat(driver, "//input[contains(@class,'entity_vrsPlat')]");
-	}
-
-	/// Objetivo: Preencher o campo Versao CA com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorVersaoCA() {
-		// Preencher Combobox
 		writeversionca(driver, "//input[contains(@class,'entity_vrsCA')]");
-	}
-
-	/// Objetivo: Preencher o text area Pan Web com parte do nome.
+		}
+	
+	/// Objetivo: Preencher o Campos dos 4 campos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorPanWeb() {
-		// Prencher Período
+	public void TestNovosCampos2() {
+		// Preencher Campos
 		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
+		clickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
 		writeperiod1x(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
 		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
 		clickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
 		writeperiod2x(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
 		// Preencher novos campos
+		waitingtoclickx(driver, "//input[contains(@class,'entity_panWeb')]");
+		clickx(driver, "//input[contains(@class,'entity_panWeb')]");
 		writepanx(driver, "//input[contains(@class,'entity_panWeb')]");
-		}
-
-	/// Objetivo: Preencher o text area CVV com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-	
-	@Test
-	public void TestEmissorCVV() {
-		// Preencher Período
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		writeperiod1x(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		clickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		writeperiod2x(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		// Preencher novos campos
 		writecvvx(driver, "//input[contains(@class,'entity_cvv')]");
-		}
-
-	/// Objetivo: Preencher o text area Descriptor com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-	
-	@Test
-	public void TestEmissorDescriptor() {
-		// Preencher Período
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		writeperiod1x(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		clickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		writeperiod2x(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		// Preencher novos campos
 		writedescx(driver, "//input[contains(@class,'entity_descriptor')]");
-		}
-
-	/// Objetivo: Preencher o text area Portador com parte do nome.
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-	
-	@Test
-	public void TestEmissorPortador() {
-		// Preencher Período
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		writeperiod1x(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
-		waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		clickx(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		writeperiod2x(driver, "//input[contains(@id,'dt_transaction_fin_component_input')]");
-		// Preencher novos campos
 		writeportx(driver, "//input[contains(@class,'entity_portador')]");
 		}
-
-	/// Objetivo: Preencher o combo Tipo de Venda
+	
+	/// Objetivo: Preencher o Campos dos 4 campos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorTipoDeVenda() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_SaleType')]");
+	public void TestCombos3() {
+		// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_SaleType')]");
-		clickx(driver, "//div[37]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Meio de captura
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorMeioDeCaptura() {
-		// Preencher Combobox
+		clickx(driver, "//div[34]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_DeviceCapture')]");
 		clickx(driver, "//div[contains(@class,'entity_DeviceCapture')]");
-		clickx(driver, "//div[38]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Modo de Entrada
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorModoDeEntrada() {
-		// Preencher Combobox
+		clickx(driver, "//div[35]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_EnterMode')]");
 		clickx(driver, "//div[contains(@class,'entity_EnterMode')]");
-		clickx(driver, "//div[39]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Grupo Ramo Atividade
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorGrupoRamoAtividade() {
-		// Preencher Combobox
+		clickx(driver, "//div[36]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_PrincipalBusiness')]");
 		clickx(driver, "//div[contains(@class,'entity_PrincipalBusiness')]");
-		clickx(driver, "//div[40]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Tipo de Emissor
+		clickx(driver, "//div[37]/div[1]/div[1]");
+		}
+	
+	/// Objetivo: Preencher os Combos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorTipoDeEmissor() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_IssuerType')]");
+	public void TestCombos4() {
+		// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_IssuerType')]");
-		clickx(driver, "//div[41]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Tipo de Transacao
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-	@Test
-	public void TestEmissorTipoDeTransacao() {
-		WebDriver driver = new ChromeDriver();
-		// Preencher Combobox
+		clickx(driver, "//div[38]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_TypeTransaction')]");
 		clickx(driver, "//div[contains(@class,'entity_TypeTransaction')]");
-		clickx(driver, "//div[42]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Origens Transacoes Web
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-	@Test
-	public void TestEmissorOrigensTrancoesWeb() {
-		// Preencher Combobox
+		clickx(driver, "//div[39]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_WebSourceTransaction')]");
 		clickx(driver, "//div[contains(@class,'entity_WebSourceTransaction')]");
-		clickx(driver, "//div[44]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Codigo da Empresa Adquirente
+		clickx(driver, "//div[41]/div[1]/div[1]");
+		}
+	
+	/// Objetivo: Preencher os Combos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+		
 	@Test
-	public void TestEmissorCodigoEmpresaAdquirente() {
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_CompanyCodePurchasing')]");
+	public void TestCombos5() {
+		// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_CompanyCodePurchasing')]");
-		clickx(driver, "//div[45]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Codigo Motivo Rede Externa
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodigoMotivoRedeExterna() {
-		// Preencher Combobox
+		clickx(driver, "//div[42]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_ReasonCodeExternalNetwork')]");
 		clickx(driver, "//div[contains(@class,'entity_ReasonCodeExternalNetwork')]");
-		clickx(driver, "//div[46]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Switch
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorSwitch() {
-		// Preencher Combobox
+		clickx(driver, "//div[43]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_Switch')]");
 		clickx(driver, "//div[contains(@class,'entity_Switch')]");
-		clickx(driver, "//div[47]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo FrontEnd
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorFrontEnd() {
-		// Preencher Combobox
+		clickx(driver, "//div[44]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_Frontend')]");
 		clickx(driver, "//div[contains(@class,'entity_Frontend')]");
-		clickx(driver, "//div[48]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Codigo de Servico
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodServico() {
-		// Preencher Combobox
+		clickx(driver, "//div[45]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_ServiceCode')]");
 		clickx(driver, "//div[contains(@class,'entity_ServiceCode')]");
-		clickx(driver, "//div[49]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Transacoes Tokenizadas
+		clickx(driver, "//div[46]/div[1]/div[1]");
+		}
+	
+	/// Objetivo: Preencher os Combos
 	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
+	
 	@Test
-	public void TestEmissorTransacoesTokenizadas() {
-		WebDriver driver = new ChromeDriver();
-		// Preencher Combobox
-		waitingtoclickx(driver, "//div[contains(@class,'entity_TokenTransaction')]");
+	public void TestCombos6() {
+		// Preencher primeiros combos
 		clickx(driver, "//div[contains(@class,'entity_TokenTransaction')]");
-		clickx(driver, "//div[50]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Transacoes Tokenizadas
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorQRCode() {
-		// Preencher Combobox
+		clickx(driver, "//div[47]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_QrCode')]");
 		clickx(driver, "//div[contains(@class,'entity_QrCode')]");
-		clickx(driver, "//div[51]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Codigo Carteira
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorCodigoCarteira() {
-		// Preencher Combobox
+		clickx(driver, "//div[48]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_Wallet')]");
 		clickx(driver, "//div[contains(@class,'entity_Wallet')]");
-		clickx(driver, "//div[52]/div[1]/div[1]");
-	}
-
-	/// Objetivo: Preencher o combo Grupo de Bandeira
-	/// Resultado Esperado: Verificar se esta retornando dados e nao ocorreu nenhum erro administrativo.
-
-	@Test
-	public void TestEmissorGrupodeBandeira() {
-		// Preencher Combobox
+		clickx(driver, "//div[49]/div[1]/div[1]");
 		waitingtoclickx(driver, "//div[contains(@class,'entity_BrandGroup')]");
 		clickx(driver, "//div[contains(@class,'entity_BrandGroup')]");
-		clickx(driver, "//div[53]/div[1]/div[1]");
-	}
+		clickx(driver, "//div[50]/div[1]/div[1]");
+		}
 }
