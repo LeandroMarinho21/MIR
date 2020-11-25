0db0776e9ea3dbe50760f0ace72519becf5900a9
LeandroMarinho21 <leandro.tmarinho21@gmail.com>
2020-11-25 11:34:30 -0300

    Update FiltrosTiposDeTransacoes.java

--- src/test/java/visao/tipos/de/transacoes/FiltrosTiposDeTransacoes.java
+++ src/test/java/visao/tipos/de/transacoes/FiltrosTiposDeTransacoes.java
@@ -137,7 +137,8 @@ public class FiltrosTiposDeTransacoes extends Filtros {
 
 		waitingtoclickx(driver, "//div[contains(@class,'entity_Switch')]");
 		clickx(driver, "//div[contains(@class,'entity_Switch')]");
-		clickx(driver, "//div[42]/div[1]/div[1]");
+		clickx(driver, "//div[79]/div[2]/ul/li[1]/div/div[2]]");
+		clickx(driver, "//div[79]/div[2]/ul/li[2]/div/div[2]]");
 		}
 	
 	
