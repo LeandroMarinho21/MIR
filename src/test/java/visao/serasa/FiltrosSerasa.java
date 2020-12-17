2d751acba9160a2d827f6cdc9c6e7f1dd410d2e7
LeandroMarinho21 <leandro.tmarinho21@gmail.com>
2020-12-17 12:25:25 -0300

    Update FiltrosSerasa.java

--- src/test/java/visao/serasa/FiltrosSerasa.java
+++ src/test/java/visao/serasa/FiltrosSerasa.java
@@ -24,10 +24,10 @@ public class FiltrosSerasa extends Filtros {
 			// Login
 			login2(driver, links.get(12).getUrl());
 			// Filtro
-			waitingtoclick(driver, "bntFilter");
+			waitingtoclick(driver, "btnFilter");
 			//Verificar Errro ao entrar
 			erroDesc(driver, "//span[text()='Ocorreu um erro desconhecido, consulte o administrador']");
-			click(driver, "bntFilter");
+			click(driver, "btnFilter");
 			waiting(driver, "dialogLoadbar");
 			// Preencher Período
 			waitingtoclickx(driver, "//input[contains(@id,'dt_transaction_ini_input')]");
