283893641fc3ebeb7822fed628239c006b43b16b
Leandrolord <67149726+Leandrolord@users.noreply.github.com>
2021-01-27 17:42:48 -0300

    Update FiltrosEmissor.java

--- src/test/java/principal/volume/de/negocio/visao/emissor/FiltrosEmissor.java
+++ src/test/java/principal/volume/de/negocio/visao/emissor/FiltrosEmissor.java
@@ -61,10 +61,10 @@ public class FiltrosEmissor {
 	public void TestCombos1() {
 		// Preencher primeiros combos
 		f.filterComboEmissor();
-		f.selectAll("//div[18]/div[1]/div[1]");
+		f.selectAll("//div[10]/div[1]/div[1]");
 		f.waitToClickComboSubProduto();
 		f.filterComboSubProduto();
-		f.selectAll("//div[19]/div[1]/div[1]");
+		f.selectAll("//div[11]/div[1]/div[1]");
 		}
 		
 	/********* Objetivo: Selecionar todas as opções dos Combos ************/
@@ -74,13 +74,13 @@ public class FiltrosEmissor {
 	public void TestCombos2() {
 			// Preencher primeiros combos
 		f.filterComboParcelas();
-		f.selectAll("//div[20]/div[1]/div[1]");
+		f.selectAll("//div[12]/div[1]/div[1]");
 		f.waitToClickComboOperadoras();
 		f.filterComboOperadoras();
-		f.selectAll("//div[21]/div[1]/div[1]");
+		f.selectAll("//div[13]/div[1]/div[1]");
 		f.waitToClickComboBandeiras();
 		f.filterComboBandeiras();
-		f.selectAll("//div[22]/div[1]/div[1]");
+		f.selectAll("//div[14]/div[1]/div[1]");
 		}
 	
 	/********* Objetivo: Preencher os 11 campos por like ************/
@@ -122,16 +122,16 @@ public class FiltrosEmissor {
 	public void TestCombos3() {
 		// Preencher primeiros combos
 		f.filterComboTipoDeVenda();
-		f.selectAll("//div[23]/div[1]/div[1]");
+		f.selectAll("//div[15]/div[1]/div[1]");
 		f.waitToClickComboMeioDeCaptura();
 		f.filterComboMeioDeCaptura();
-		f.selectAll("//div[24]/div[1]/div[1]");
+		f.selectAll("//div[16]/div[1]/div[1]");
 		f.waitToClickComboModoDeEntrada();
 		f.filterComboModoDeEntrada();
-		f.selectAll("//div[25]/div[1]/div[1]");
+		f.selectAll("//div[17]/div[1]/div[1]");
 		f.waitToClickComboGrupoRamoAtividade();
 		f.filterComboGrupoRamoAtividade();
-		f.selectAll("//div[26]/div[1]/div[1]");
+		f.selectAll("//div[18]/div[1]/div[1]");
 		}
 	
 	/********* Objetivo: Selecionar todas as opções dos Combos ************/
@@ -141,13 +141,13 @@ public class FiltrosEmissor {
 	public void TestCombos4() {
 		// Preencher primeiros combos
 		f.filterComboTipoDeEmissor();
-		f.selectAll("//div[27]/div[1]/div[1]");
+		f.selectAll("//div[19]/div[1]/div[1]");
 		f.waitToClickComboTipoTransacao();
 		f.filterComboTipoTransacao();
-		f.selectAll("//div[28]/div[1]/div[1]");
+		f.selectAll("//div[20]/div[1]/div[1]");
 		f.waitToClickComboOrigemTransacoesWeb();
 		f.filterComboOrigemTransacoesWeb();
-		f.selectAll("//div[30]/div[1]/div[1]");
+		f.selectAll("//div[22]/div[1]/div[1]");
 		}
 	
 	/********* Objetivo: Selecionar todas as opções dos Combos ************/
@@ -157,19 +157,19 @@ public class FiltrosEmissor {
 	public void TestCombos5() {
 		// Preencher primeiros combos
 		f.filterComboCodEmpresaAdquirente();
-		f.selectAll("//div[31]/div[1]/div[1]");
+		f.selectAll("//div[23]/div[1]/div[1]");
 		f.waitToClickComboCodMotivoRedeExterna();
 		f.filterComboCodMotivoRedeExterna();
-		f.selectAll("//div[32]/div[1]/div[1]");
+		f.selectAll("//div[24]/div[1]/div[1]");
 		f.waitToClickComboSwitch();
 		f.filterComboSwitch();
-		f.selectAll("//div[33]/div[1]/div[1]");
+		f.selectAll("//div[25]/div[1]/div[1]");
 		f.waitToClickComboFrontEnd();
 		f.filterComboFrontEnd();
-		f.selectAll("//div[34]/div[1]/div[1]");
+		f.selectAll("//div[26]/div[1]/div[1]");
 		f.waitToClickComboCodServico();
 		f.filterComboCodServico();
-		f.selectAll("//div[35]/div[1]/div[1]");
+		f.selectAll("//div[27]/div[1]/div[1]");
 		}
 	
 	/********* Objetivo: Selecionar todas as opções dos Combos ************/
@@ -179,15 +179,15 @@ public class FiltrosEmissor {
 	public void TestCombos6() {
 		// Preencher primeiros combos
 		f.filterComboTransacoesTokenizadas();
-		f.selectAll("//div[36]/div[1]/div[1]");
+		f.selectAll("//div[28]/div[1]/div[1]");
 		f.waitToClickComboQrCode();
 		f.filterComboQrCode();
-		f.selectAll("//div[37]/div[1]/div[1]");
+		f.selectAll("//div[29]/div[1]/div[1]");
 		f.waitToClickComboCodigoCarteira();
 		f.filterComboCodigoCarteira();
-		f.selectAll("//div[38]/div[1]/div[1]");
+		f.selectAll("//div[30]/div[1]/div[1]");
 		f.waitToClickComboGrupoBandeira();
 		f.filterComboGrupoBandeira();
-		f.selectAll("//div[39]/div[1]/div[1]");
+		f.selectAll("//div[31]/div[1]/div[1]");
 		}
 }
