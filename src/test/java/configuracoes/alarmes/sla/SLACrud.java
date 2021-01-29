5e3a9ce6955c831f56da42098c44bbd505768bd0
Leandrolord <67149726+Leandrolord@users.noreply.github.com>
2021-01-29 16:30:55 -0300

    substituição de classe otimizada

--- src/test/java/configuracoes/alarmes/sla/SLACrud.java
+++ src/test/java/configuracoes/alarmes/sla/SLACrud.java
@@ -6,71 +6,68 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import metodos.mir.Filtros;
-
-
 public class SLACrud {
 	
-	private Filtros filtros;
+	
 	private WebDriver driver;
-
-
+	private SLACrudPage sla;
+	
 	@Before
 	public void Inicializar() {
 		driver = new ChromeDriver();
-		filtros = new Filtros(driver);
-		driver.get("https://192.168.80.18:8081/mir.console/pages/view/slaMilestone.jsf");
+		new MetodosSLA(driver);
+		sla = new SLACrudPage(driver);
+		sla.acessarUrl();
 		// Login
-		filtros.login2();
+		sla.logar();
 		// Entrar na tela
-		filtros.waitingtoclick("frmList:dataTableSlaMilestone:0:deleteButton");
+		sla.aguardarParaClicar();
 		//Verificar Errro ao entrar
-		filtros.erroDesc("//span[text()='Ocorreu um erro desconhecido, consulte o administrador.']");
+		sla.verificarErro();
 		}
 
 	@After
 	public void Encerramento() {
-		filtros.erroDesc("//span[text()='Ocorreu um erro desconhecido, consulte o administrador.']");
+		sla.verificarErro();
 		System.out.println("Sucess");
-		driver.quit();
+		sla.encerrar();
 		}
-
-
+	
 	@Test
 	public void Atualizar() {
 	// Clicar no elemento
-	filtros.clickx("//*[@id=\"frmList:dataTableSlaMilestone_data\"]/tr[1]/td[1]");
-	filtros.waiting("dialogLoadbar");
-	filtros.click("frmCadastro:j_idt79");
-	filtros.clear("frmCadastro:j_idt79");
-	filtros.write("frmCadastro:j_idt79", "Teste Auto");
-	filtros.obteinValue("frmCadastro:j_idt79");
+	sla.selecionarElementoTabela();
+	sla.carregando();
+	sla.clicarCampoNome();
+	sla.limparCampoNome();
+	sla.escreverCampoNome("Teste Auto");
+	sla.obterNome();
 	// Atualizar
-	filtros.click("frmCadastro:btnUpdate");
-	filtros.waiting("dialogLoadbar");
+	sla.clicarBotaoUpdate();
+	sla.carregando();
 	// Verificar se exibe mensagem
-	filtros.waitingelementxpath("//span[text()='SLA alterado com sucesso']");
+	sla.obterMensagemAlterado();
 	}
 	
 	@Test
 	public void SalvarRegras() {
 	// Clicar no elemento
-	filtros.click("frmCadastro:j_idt79");
-	filtros.write("frmCadastro:j_idt79", "Teste");
-	filtros.obteinValue("frmCadastro:j_idt79");
+	sla.clicarCampoNome();
+	sla.escreverCampoNome("Teste");
+	sla.obterNome();
 	// Salvar
-	filtros.click("frmCadastro:btnSave");
-	filtros.waiting("dialogLoadbar");
+	sla.clicarBotaoSalvar();
+	sla.carregando();
 	// Verificar se exibe mensagem
-	filtros.waitingelementxpath("//span[text()='O campo Prioridade é obrigatório.']");
-	filtros.click("frmCadastro:j_idt84");
-	filtros.write("frmCadastro:j_idt84", "4");
-	filtros.obteinValue("frmCadastro:j_idt84");
-	filtros.click("frmCadastro:j_idt89");
+	sla.obterMensagemPrioridadeObrigatorio();
+	sla.clicarCampoPrioridade();
+	sla.escreverCampoPrioridade("4");
+	sla.obterPrioridade();
 	// Salvar
-	filtros.click("frmCadastro:btnSave");
-	filtros.waiting("dialogLoadbar");
+	sla.clicarBotaoSalvar();
+	sla.carregando();
 	// Verificar se exibe mensagem
-	filtros.waitingelementxpath("//span[text()='O campo Cor é obrigatório.']");
+	sla.obterMensagemCorObrigatorio();
 	}
+
 }
